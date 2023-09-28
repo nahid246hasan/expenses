bb58b52e07149a8e78ec7d42daa7c38f4f72f7af
nahid246hasan <nahid246hasan@gmail.com>
2023-09-28 17:13:53 +0600

    Initial

---
+++ lib/expenses.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    // TODO: implement createState
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:expence_tracker/expenses.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    )
+  );
+}
